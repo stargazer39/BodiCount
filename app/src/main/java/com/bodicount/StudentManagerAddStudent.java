0f9a0b6ca13f72724fdf1d700de223100b27e287
sewwandiWM <it20298494@my.sliit.lk>
2022-03-20 11:58:02 +0530

    Sewwandi's part - Incomplete

---
+++ app/src/main/java/com/bodicount/StudentManagerAddStudent.java
@@ -0,0 +1,14 @@
+package com.bodicount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentManagerAddStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_manager_add_student);
+    }
+}
