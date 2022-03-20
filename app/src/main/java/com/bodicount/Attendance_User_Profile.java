3d89f922b2778bc08f39d0f59bd6acdaf12378b1
nithular <nithularatnayaker@gmail.com>
2022-03-20 12:29:06 +0530

    Nithulas part - Incomplete

---
+++ app/src/main/java/com/bodicount/Attendance_User_Profile.java
@@ -0,0 +1,14 @@
+package com.bodicount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Attendance_User_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_user_profile);
+    }
+}
