9204791a25bc363c75f58c7ea51e10f79e64e3e7
nithular <nithularatnayaker@gmail.com>
2022-04-26 18:53:00 +0530

    Student Login

---
+++ app/src/main/java/com/bodicount/Organizer_Student_Login.java
@@ -0,0 +1,15 @@
+package com.bodicount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Organizer_Student_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizer_student_login);
+    }
+
+}
