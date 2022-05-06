3b5b2a1dc9cb562252934306f3e9879ef5a1807a
nithular <nithularatnayaker@gmail.com>
2022-05-06 16:26:28 +0530

    Homepage Updates

--- app/src/main/java/com/bodicount/Attendance_homepage.java
+++ app/src/main/java/com/bodicount/Attendance_homepage.java
@@ -2,7 +2,11 @@ package com.bodicount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.bodicount.student.Attendance_User_Profile;
 
 public class Attendance_homepage extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class Attendance_homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_homepage);
+    }
+
+    public void goToUserProfile(View view) {
+        Intent intent = new Intent(this, Attendance_User_Profile.class);
+        startActivity(intent);
     }
 }
