aa2f5357764d35fed9e8c5f343ec497e3ec99ad0
nithular <nithularatnayaker@gmail.com>
2022-05-06 16:30:31 +0530

    User profile edit changes

--- app/src/main/java/com/bodicount/student/Attendance_user_profile_edit.java
+++ app/src/main/java/com/bodicount/student/Attendance_user_profile_edit.java
@@ -1,8 +1,10 @@
-package com.bodicount;
+package com.bodicount.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.bodicount.R;
 
 public class Attendance_user_profile_edit extends AppCompatActivity {
 
