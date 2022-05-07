cd34ad18b5b4666744f2f443c04eaf748601d385
nithular <nithularatnayaker@gmail.com>
2022-05-07 23:30:42 +0530

    Updated Student.java

--- app/src/main/java/com/bodicount/student/Student.java
+++ app/src/main/java/com/bodicount/student/Student.java
@@ -4,6 +4,8 @@ import com.bodicount.commons.User;
 import com.bodicount.commons.UserTypes;
 
 public class Student extends User {
+    private String organizerID;
+    private String image;
     public Student () {
         super();
         this.type = UserTypes.STUDENT;
@@ -11,5 +13,19 @@ public class Student extends User {
     @Override
     public String getType() {
         return UserTypes.STUDENT;
+    }
+
+    public String getImage() { return image; }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOrganizerID() {
+        return organizerID;
+    }
+
+    public void setOrganizerID(String organizerID) {
+        this.organizerID = organizerID;
     }
 }
