809a2c1fa49c3f63b7a9b9585d6e9b23a1b83b84
Nuwanthi Rathnayake <101658210+NuwanthiRathnayake@users.noreply.github.com>
2022-05-08 11:18:21 +0530

    students Notes UI

---
+++ app/src/main/java/com/bodicount/studentsnotes/StudentNotesUserView.java
@@ -0,0 +1,16 @@
+package com.bodicount.studentsnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bodicount.R;
+
+public class StudentNotesUserView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_notes_user_view);
+    }
+}
