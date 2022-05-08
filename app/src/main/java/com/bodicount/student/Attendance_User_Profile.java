c669c93a7a7f84cdec0cc0caff6e510f173da2c1
nithular <nithularatnayaker@gmail.com>
2022-05-08 16:44:44 +0530

    Latest Changes

--- app/src/main/java/com/bodicount/student/Attendance_User_Profile.java
+++ app/src/main/java/com/bodicount/student/Attendance_User_Profile.java
@@ -1,5 +1,6 @@
 package com.bodicount.student;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,15 +10,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bodicount.Helpers4Nithula;
 import com.bodicount.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class Attendance_User_Profile extends AppCompatActivity {
 
@@ -80,31 +87,57 @@ public class Attendance_User_Profile extends AppCompatActivity {
     }
 
 
+    private void showToast(String message, int duration){
+        Toast toast = Toast.makeText(this, message, duration);
+        toast.show();
+    }
+
 
     public void deleteData(View view){
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        sAuth = FirebaseAuth.getInstance();
-//        user = sAuth.getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        sAuth = FirebaseAuth.getInstance();
+        String user = sAuth.getCurrentUser().getUid();
 
 //        new AlertDialog.Builder(getApplicationContext())
 //                .setTitle("Delete Account".toUpperCase(Locale.ROOT))
 //                .setMessage("Do you really want to delete your account?")
 //                .setIcon(android.R.drawable.ic_dialog_alert)
 //                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
-        db.collection("user").document(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                .delete().addOnCompleteListener(task -> {
-
-                    if(task.isSuccessful()){
+//        db.collection("user").document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                .delete("o").addOnCompleteListener(task -> {
 //
-//                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+//                    if(task.isSuccessful()){
+////
+////                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+//                    }
+//        });
+
+
+        Map<String, Object> unsubscribe = new HashMap<>();
+        unsubscribe.put("organizerID", FieldValue.delete());
+        FirebaseFirestore.getInstance()
+                .collection("user")
+                .document(user)
+                .update(unsubscribe).addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        showToast("Successfully unsubscribed from Organization", Toast.LENGTH_SHORT);
+                    }else{
+                        showToast("Unsubscription Unsuccessful", Toast.LENGTH_SHORT);
                     }
         });
-
     }
 
     public void goToEdit(View view){
         Intent intent = new Intent(this , Attendance_user_profile_edit.class);
         startActivity(intent);
+    }
+
+    public void logout(View view){
+
+        sAuth.getInstance().signOut();
+        Intent intent = new Intent(this, Attendance_Student_Login.class);
+        startActivity(intent);
+
     }
 
 }
