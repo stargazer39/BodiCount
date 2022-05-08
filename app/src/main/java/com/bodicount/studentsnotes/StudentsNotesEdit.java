50443718a2f5a797ba17b30d55fda0cf6fbff405
Nuwanthi Rathnayake <101658210+NuwanthiRathnayake@users.noreply.github.com>
2022-05-08 11:24:33 +0530

    notes edit

---
+++ app/src/main/java/com/bodicount/studentsnotes/StudentsNotesEdit.java
@@ -0,0 +1,16 @@
+package com.bodicount.studentsnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bodicount.R;
+
+public class StudentsNotesEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_notes_edit);
+    }
+}
