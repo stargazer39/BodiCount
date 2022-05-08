719919cb0df9c0f8d38f001e5c4b72e39cdbac61
Nuwanthi Rathnayake <101658210+NuwanthiRathnayake@users.noreply.github.com>
2022-05-08 11:21:18 +0530

    notes search

---
+++ app/src/main/java/com/bodicount/studentsnotes/StudentSearch.java
@@ -0,0 +1,16 @@
+package com.bodicount.studentsnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bodicount.R;
+
+public class StudentSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_search);
+    }
+}
