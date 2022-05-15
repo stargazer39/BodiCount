b6d5c6b317a8c2892fee3150c3a78d2c171aa26c
StargazeR Weerakkody <dehemiweerakkodi2@gmail.com>
2022-05-15 15:39:18 +0530

    Implement timeslots in student homepage

--- app/src/main/java/com/bodicount/Attendance_homepage.java
+++ app/src/main/java/com/bodicount/Attendance_homepage.java
@@ -8,27 +8,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.bodicount.commons.Attendance;
 import com.bodicount.student.Attendance_User_Profile;
 import com.bodicount.student.Student;
 import com.bodicount.student.attendanceMarkingOnline;
 import com.bodicount.studentsnotes.StudentNotesManagement;
+import com.bodicount.timeslot.Timeslot;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 public class Attendance_homepage extends AppCompatActivity {
     private FirebaseAuth sAuth;
     private FirebaseUser user;
     private FirebaseFirestore db;
+    private Button nowButton;
+    private Button nextButton;
+    private Button nextAfterButton;
 
     private Student currentStudent;
     @Override
@@ -57,7 +77,9 @@ public class Attendance_homepage extends AppCompatActivity {
         };
         handler.postDelayed(runnable, 1000);
 
-
+        nowButton = findViewById(R.id.nowTslot);
+        nextButton = findViewById(R.id.nextTslot);
+        nextAfterButton = findViewById(R.id.nextAfterTslot);
 
         db.collection("user")
                 .document(sAuth.getUid())
@@ -72,7 +94,87 @@ public class Attendance_homepage extends AppCompatActivity {
                                     .collection("timetables")
                                     .document(currentStudent.getTimetableID())
                                     .collection("timeslots")
-                                    .document();
+                                    .get()
+                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if(task.isSuccessful()){
+                                                List<Timeslot> timeslotList = task.getResult().toObjects(Timeslot.class);
+                                                // Get the today's time slots
+                                                DateTime tNow = DateTime.now();
+                                                LocalTime timeNow = tNow.toLocalTime();
+
+                                                int today = tNow.getDayOfWeek();
+
+                                                Iterator<Timeslot> i = timeslotList.iterator();
+                                                while (i.hasNext()) {
+                                                    Timeslot tThis = i.next();
+                                                    if(tThis.getDate() != today)
+                                                        i.remove();
+                                                }
+
+                                                // Sort by time
+                                                Collections.sort(timeslotList, new Comparator<Timeslot>() {
+                                                    @Override
+                                                    public int compare(Timeslot t1, Timeslot t2) {
+                                                        LocalTime startT1 = Helpers4Dehemi.parseLocalTime(t1.getStartTime());
+                                                        LocalTime startT2 = Helpers4Dehemi.parseLocalTime(t2.getStartTime());
+                                                        return startT1.compareTo(startT2);
+                                                    }
+                                                });
+
+                                                // Get current timeslot
+                                                Iterator<Timeslot> k = timeslotList.iterator();
+                                                Timeslot before = null;
+                                                Timeslot now = null;
+                                                Timeslot later = null;
+                                                Timeslot laterAfter = null;
+
+                                                while (k.hasNext()) {
+                                                    Timeslot tThis = k.next();
+                                                    LocalTime start = Helpers4Dehemi.parseLocalTime(tThis.getStartTime());
+                                                    LocalTime end = Helpers4Dehemi.parseLocalTime(tThis.getEndTime());
+
+                                                    if(start.isEqual(timeNow) || (start.isBefore(timeNow) && end.isAfter(timeNow))) {
+                                                        now = tThis;
+
+                                                        if(k.hasNext()){
+                                                            later = k.next();
+
+                                                            if(k.hasNext()){
+                                                                laterAfter = k.next();
+                                                            }
+                                                        }
+                                                        break;
+                                                    }
+
+                                                    before = tThis;
+                                                }
+
+                                                // Show it in the UI
+                                                if(now != null){
+                                                    nowButton.setText(now.getSlotName() + "\n" + Helpers4Dehemi.timeslotToString(now));
+                                                }else{
+                                                    nowButton.setText("Nothing to panic right now.");
+                                                }
+
+                                                if(later != null){
+                                                    nextButton.setText(later.getSlotName() + "\n" + Helpers4Dehemi.timeslotToString(later));
+                                                }else{
+                                                    nextButton.setText("Nothing next.");
+                                                }
+
+                                                if(laterAfter != null){
+                                                    nextAfterButton.setText(laterAfter.getSlotName() + "\n" + Helpers4Dehemi.timeslotToString(laterAfter));
+                                                }else{
+                                                    nextAfterButton.setText("Nothing later.");
+                                                }
+
+                                            }else{
+                                                task.getException().printStackTrace();
+                                            }
+                                        }
+                                    });
                         }else{
                             task.getException().printStackTrace();
                         }
--- app/src/main/java/com/bodicount/Helpers4Dehemi.java
+++ app/src/main/java/com/bodicount/Helpers4Dehemi.java
@@ -3,11 +3,18 @@ package com.bodicount;
 import android.view.View;
 import android.widget.EditText;
 
+import com.bodicount.timeslot.Timeslot;
+
 import org.joda.time.DateTimeConstants;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class Helpers4Dehemi {
     public static String[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
     public static int[] dayConstant = { DateTimeConstants.MONDAY, DateTimeConstants.TUESDAY, DateTimeConstants.WEDNESDAY, DateTimeConstants.THURSDAY, DateTimeConstants.FRIDAY, DateTimeConstants.SATURDAY, DateTimeConstants.SUNDAY };
+    private static DateTimeFormatter fmt = DateTimeFormat.forPattern("HH:mm:ss.SSSSS");
+    private static DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("hh:mm a ");
 
     public static String vaildateString(String str, boolean trim, String tag) throws Exception {
         String new_string;
@@ -30,5 +37,16 @@ public class Helpers4Dehemi {
 
     public static long convertToMillis(int hour, int minute){
         return  (hour*60*60 + minute*60)*1000;
+    }
+
+    public static LocalTime parseLocalTime(String timeString){
+        return fmt.parseLocalTime(timeString);
+    }
+
+    public static String timeslotToString(Timeslot timeslot){
+        LocalTime sTime = parseLocalTime(timeslot.getStartTime());
+        LocalTime eTime = parseLocalTime(timeslot.getEndTime());
+
+        return "From - " + sTime.toString(timeFormatter) + " To " + eTime.toString(timeFormatter).toString();
     }
 }
